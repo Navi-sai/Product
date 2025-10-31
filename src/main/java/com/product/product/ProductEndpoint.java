6257c4a1355c884d4716727dd3617ed22f1ff519
Navi-sai <vaishnavi3990@gmail.com>
2025-10-31 20:17:09 +0530

    Second

--- src/main/java/com/product/product/ProductEndpoint.java
+++ src/main/java/com/product/product/ProductEndpoint.java
@@ -17,7 +17,7 @@ public class ProductEndpoint {
 		ProductResponse response = new ProductResponse();
 		response.setName(request.getName().toUpperCase());
 		response.setColor(request.getColor().toUpperCase());
-		System.out.println("Program Completed - 10");
+		System.out.println("Program Completed - 20");
 
 		return response;
 	}
